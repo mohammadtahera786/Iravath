972cf96d2ada3582bc15b8def2e9cff44bd4b3a7
Mohammad Tahera <mohammadtahera786@users.noreply.github.com>
2026-05-06 17:46:15 +0530

    Initial commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div className="w-full h-full" gap->
+      <button className= "flex bg-red-600 text-2xl text-red mt-4 cursor-pointer undeline "> click</button>
+      <h1/>
+    </div>
+  )
+}
+export default App;
